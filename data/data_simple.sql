764f13c29c56954e47aa0a1d57992f9c773a1e9b
JBinggi <juerg.binggeli@gmail.com>
2020-02-09 09:33:54 +0100

    Add state entities

--- data/data_simple.sql
+++ data/data_simple.sql
@@ -1,10 +1,16 @@
 --
--- add type fields
+-- add Core Fields
 --
 INSERT INTO `core_form_field` (`Field_ID`, `type`, `label`, `fieldkey`, `tab`, `form`, `class`, `url_view`, `url_list`, `show_widget_left`, `allow_clear`, `readonly`, `tbl_cached_name`, `tbl_class`, `tbl_permission`) VALUES
 (NULL, 'select', 'Article', 'article_idfs', 'request-base', 'contactrequest-single', 'col-md-3', '', '/article/api/list/0', 0, 1, 0, 'entitytag-single', 'OnePlace\\Article\\Model\\ArticleTable','add-OnePlace\\Article\\Controller\\ArticleController'),
-(NULL, 'select', 'State', 'state_idfs', 'request-base', 'contactrequest-single', 'col-md-3', '', '/tag/api/list/task-single/state', 0, 1, 0, 'entitytag-single', 'OnePlace\\Tag\\Model\\EntityTagTable', 'add-OnePlace\\Contact\\Request\\\\Controller\\StateController');
+(NULL, 'select', 'State', 'state_idfs', 'request-base', 'contactrequest-single', 'col-md-3', '', '/tag/api/list/contactrequest-single/state', 0, 1, 0, 'entitytag-single', 'OnePlace\\Tag\\Model\\EntityTagTable', 'add-OnePlace\\Contact\\Request\\\\Controller\\StateController');
 
+--
+-- add State Entities
+--
+INSERT INTO `core_entity_tag` (`Entitytag_ID`, `entity_form_idfs`, `tag_idfs`, `tag_value`, `parent_tag_idfs`, `created_by`, `created_date`, `modified_by`, `modified_date`) VALUES
+(NULL, 'contactrequest-single', '2', 'new', '0', '1', '2020-01-01 00:00:00', '1', '2020-01-01 00:00:00'),
+(NULL, 'contactrequest-single', '2', 'closed', '0', '1', '2020-01-01 00:00:00', '1', '2020-01-01 00:00:00');
 
 --
 -- permissions
